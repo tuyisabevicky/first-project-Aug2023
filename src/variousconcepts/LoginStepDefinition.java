7f792b6234f4e3f7cec961dbe26e32848af41c9d
Victoire Tuyisabe <tuyisabevicky@gmail.com>
2024-06-04 15:55:34 -0400

    commited

---
+++ src/variousconcepts/LoginStepDefinition.java
@@ -0,0 +1,17 @@
+package variousconcepts;
+public class LoginStepDefinition{
+	
+}
+package pages;
+import java . io.File;
+
+public class TestBase{
+	public static void
+	public static WebDriver driver;
+	
+}
+		
+		
+	
+	
+	
---
+++ src/variousconcepts/WebDriver.java
@@ -0,0 +1,5 @@
+package variousconcepts;
+
+public class WebDriver {
+
+} 
